1667742c9e9a72cb91146f5968fad42916dfe686
Felipe Crescencio De Oliveira <felipe.crescencio@jv01.local>
2017-12-31 20:24:10 -0200

    Bot catch ask for coffee and answer

--- src/main/java/com/felipecrescencio/bot/CafeTiTotvsBot.java
+++ src/main/java/com/felipecrescencio/bot/CafeTiTotvsBot.java
@@ -6,33 +6,34 @@ import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
 
 public class CafeTiTotvsBot extends TelegramLongPollingBot {
-    @Override
-    public void onUpdateReceived(Update update) {
-        // We check if the update has a message and the message has text
-        if (update.hasMessage() && update.getMessage().hasText()) {
-        	String s = update.getMessage().getText();
-        	if(update.getMessage().getText().equalsIgnoreCase("Aline chata"))
-        		s = "Mentira, a Aline é legal.";
-        	
-            SendMessage message = new SendMessage() // Create a SendMessage object with mandatory fields
-                    .setChatId(update.getMessage().getChatId())
-                    .setText(s);
+	@Override
+	public void onUpdateReceived(Update update) {
+		// We check if the update has a message and the message has text
+		if (update.hasMessage() && update.getMessage().hasText()) {
+			String message2 = update.getMessage().getText().toLowerCase().trim(); 
+			if(message2.indexOf("cafe dessa semana") >= 0) {
+				String s = "O café dessa semana é da Aline.";
 
-            try {
-                execute(message); // Call method to send the message
-            } catch (TelegramApiException e) {
-                e.printStackTrace();
-            }
-        }
-    }
+				SendMessage message = new SendMessage() // Create a SendMessage object with mandatory fields
+						.setChatId(update.getMessage().getChatId())
+						.setText(s);
 
-    @Override
-    public String getBotUsername() {
-        return "cafetitotvsbot";
-    }
+				try {
+					execute(message); // Call method to send the message
+				} catch (TelegramApiException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
 
-    @Override
-    public String getBotToken() {
-        return "521421948:AAGXUg8X9inlLDaBpxvO0Rz5SRwmiJBEhS8";
-    }
+	@Override
+	public String getBotUsername() {
+		return "cafetitotvsbot";
+	}
+
+	@Override
+	public String getBotToken() {
+		return "521421948:AAGXUg8X9inlLDaBpxvO0Rz5SRwmiJBEhS8";
+	}
 }
--- src/main/java/com/felipecrescencio/main/Main.java
+++ src/main/java/com/felipecrescencio/main/Main.java
@@ -16,9 +16,20 @@
 
 package com.felipecrescencio.main;
 
-import com.felipecrescencio.bot.CafeTiTotvsBot;
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Map;
+
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
@@ -30,73 +41,119 @@ import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.TelegramBotsApi;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
 
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.Map;
+import com.felipecrescencio.bot.CafeTiTotvsBot;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
 @Controller
 @SpringBootApplication
 public class Main {
 
-  @Value("${spring.datasource.url}")
-  private String dbUrl;
+	@Value("${spring.datasource.url}")
+	private String dbUrl;
 
-  @Autowired
-  private DataSource dataSource;
+	@Autowired
+	private DataSource dataSource;
 
-  public static void main(String[] args) throws Exception {
-    SpringApplication.run(Main.class, args);
-    
-    ApiContextInitializer.init();
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(Main.class, args);
 
-    TelegramBotsApi botsApi = new TelegramBotsApi();
+		ApiContextInitializer.init();
 
-    try {
-        botsApi.registerBot(new CafeTiTotvsBot());
-    } catch (TelegramApiException e) {
-        e.printStackTrace();
-    }
-  }
+		TelegramBotsApi botsApi = new TelegramBotsApi();
 
-  @RequestMapping("/")
-  String index() {
-    return "index";
-  }
+		try {
+			botsApi.registerBot(new CafeTiTotvsBot());
+		} catch (TelegramApiException e) {
+			e.printStackTrace();
+		}
 
-  @RequestMapping("/db")
-  String db(Map<String, Object> model) {
-    try (Connection connection = dataSource.getConnection()) {
-      Statement stmt = connection.createStatement();
-      stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
-      stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
-      ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
 
-      ArrayList<String> output = new ArrayList<String>();
-      while (rs.next()) {
-        output.add("Read from DB: " + rs.getTimestamp("tick"));
-      }
+		// run each 25 min
+		final long timeInterval = 1500000;
+		Runnable runnable = new Runnable() {
 
-      model.put("records", output);
-      return "db";
-    } catch (Exception e) {
-      model.put("message", e.getMessage());
-      return "error";
-    }
-  }
+			public void run() {
+				while (true) {
+					try {
+						sendGET();
+						System.out.println("GET DONE");
+						Thread.sleep(timeInterval);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			}
+		};
 
-  @Bean
-  public DataSource dataSource() throws SQLException {
-    if (dbUrl == null || dbUrl.isEmpty()) {
-      return new HikariDataSource();
-    } else {
-      HikariConfig config = new HikariConfig();
-      config.setJdbcUrl(dbUrl);
-      return new HikariDataSource(config);
-    }
-  }
+		Thread thread = new Thread(runnable);
+		thread.start();
+	}
+
+	private static void sendGET() throws IOException {
+		URL obj = new URL("https://cafe-ti-totvs.herokuapp.com/");
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+		con.setRequestMethod("GET");
+		con.setRequestProperty("User-Agent", "Mozilla/5.0");
+		int responseCode = con.getResponseCode();
+		System.out.println("GET Response Code :: " + responseCode);
+		if (responseCode == HttpURLConnection.HTTP_OK) { // success
+			BufferedReader in = new BufferedReader(new InputStreamReader(
+					con.getInputStream()));
+			String inputLine;
+			StringBuffer response = new StringBuffer();
+
+			while ((inputLine = in.readLine()) != null) {
+				response.append(inputLine);
+			}
+			in.close();
+
+			// print result
+			System.out.println(response.toString());
+		} else {
+			System.out.println("GET request not worked");
+		}
+
+	}
+
+	@RequestMapping("/")
+	String index() {
+		return "index";
+	}
+
+	@RequestMapping("/db")
+	String db(Map<String, Object> model) {
+		try (Connection connection = dataSource.getConnection()) {
+			Statement stmt = connection.createStatement();
+			stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
+			stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+			ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
+
+			ArrayList<String> output = new ArrayList<String>();
+			while (rs.next()) {
+				output.add("Read from DB: " + rs.getTimestamp("tick"));
+			}
+
+			model.put("records", output);
+			return "db";
+		} catch (Exception e) {
+			model.put("message", e.getMessage());
+			return "error";
+		}
+	}
+
+	@Bean
+	public DataSource dataSource() throws SQLException {
+		if (dbUrl == null || dbUrl.isEmpty()) {
+			return new HikariDataSource();
+		} else {
+			HikariConfig config = new HikariConfig();
+			config.setJdbcUrl(dbUrl);
+			return new HikariDataSource(config);
+		}
+	}
 
 }
