eb5bd96a13a3a9c1ed62c9e27deb308d964dbcff
Felipe Crescencio De Oliveira <felipe.crescencio@jv01.local>
2018-01-03 23:23:31 -0200

    Fix bot to find person who will bring coffee

--- src/main/java/com/felipecrescencio/bot/CafeTiTotvsBot.java
+++ src/main/java/com/felipecrescencio/bot/CafeTiTotvsBot.java
@@ -1,18 +1,29 @@
 package com.felipecrescencio.bot;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.telegram.telegrambots.api.methods.send.SendMessage;
 import org.telegram.telegrambots.api.objects.Update;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
 
+import com.felipecrescencio.entity.WeeklyCoffee;
+import com.felipecrescencio.service.WeeklyCoffeeService;
+
 public class CafeTiTotvsBot extends TelegramLongPollingBot {
+    @Autowired
+    WeeklyCoffeeService weeklyCoffeeService;
+	
 	@Override
 	public void onUpdateReceived(Update update) {
 		// We check if the update has a message and the message has text
 		if (update.hasMessage() && update.getMessage().hasText()) {
 			String message2 = update.getMessage().getText().toLowerCase().trim(); 
 			if((message2.indexOf("cafe") >= 0 || message2.indexOf("café") >= 0) && message2.indexOf("semana") >= 0) {
-				String s = "O café dessa semana é da Aline.";
+				List<WeeklyCoffee> lwc = weeklyCoffeeService.findAll();
+
+				String s = "O café dessa semana é do(a) "+ lwc.get(0).getName() +".";
 
 				SendMessage message = new SendMessage() // Create a SendMessage object with mandatory fields
 						.setChatId(update.getMessage().getChatId())
